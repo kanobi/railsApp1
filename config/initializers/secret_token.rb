4d6c6eb82032150a368569702cf9809966eee77e
kanobi <e.robin154@gmail.com>
2013-10-06 18:22:37 +0200

    Prvi commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrvaAp::Application.config.secret_token = '70faf5f9742caab0dad962a5d0d37268b971e5ac2e1fbee40f863b391360602cf39794e913bd47364145ad7ac1b31a32f1d50f0083d58e5fab89df246c2f8ab6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrvaAp::Application.config.session_store :cookie_store, key: '_prva_ap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrvaAp::Application.config.session_store :active_record_store
